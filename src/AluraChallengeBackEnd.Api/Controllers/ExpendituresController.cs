de73bac810f53ef4ecf5374c96c12709ab9e1398
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-08-14 22:44:27 -0300

    Created the ExpendituresController

---
+++ src/AluraChallengeBackEnd.Api/Controllers/ExpendituresController.cs
@@ -0,0 +1,66 @@
+namespace AluraChallengeBackEnd.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ExpendituresController : ControllerBase
+{
+    private readonly IExpenditureRepository _expenditureRepository;
+    private readonly IMapper _mapper;
+
+    public ExpendituresController(IExpenditureRepository expenditureRepository, IMapper mapper)
+    {
+        _expenditureRepository = expenditureRepository;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<ExpenditureViewModel>> GetAll() =>
+        _mapper.Map<IEnumerable<ExpenditureViewModel>>(await _expenditureRepository.GetAllAsync());
+
+    [HttpGet("{id:Guid}")]
+    public async Task<ActionResult<ExpenditureViewModel>> Get(Guid id)
+    {
+        var expenditureViewModel = _mapper.Map<ExpenditureViewModel>(await _expenditureRepository.GetAsync(id));
+        if (expenditureViewModel is null) NotFound();
+
+        return Ok(expenditureViewModel);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ExpenditureViewModel>> Save(ExpenditureViewModel expenditureViewModel)
+    {
+        if (!ModelState.IsValid) return BadRequest();
+
+        _expenditureRepository.Save(_mapper.Map<Expenditure>(expenditureViewModel));
+        await CommitAsync();
+
+        return Ok(expenditureViewModel);
+    }
+
+    [HttpPut("{id:Guid}")]
+    public async Task<ActionResult<IncomeViewModel>> Edit(Guid id, ExpenditureViewModel expenditureViewModel)
+    {
+        if (id != expenditureViewModel.Id) return BadRequest();
+
+        if (!ModelState.IsValid) return BadRequest();
+
+        _expenditureRepository.Edit(_mapper.Map<Expenditure>(expenditureViewModel));
+        await CommitAsync();
+
+        return Ok(expenditureViewModel);
+    }
+
+    [HttpDelete("{id:Guid}")]
+    public async Task<ActionResult> Delete(Guid id)
+    {
+        var expenditureFromDb = await _expenditureRepository.GetAsync(id);
+        if (expenditureFromDb is null) return NotFound();
+
+        _expenditureRepository.Delete(expenditureFromDb);
+        await CommitAsync();
+        
+        return Ok();
+    }
+
+    private async Task<bool> CommitAsync() => await _expenditureRepository.UnitOfWork.CommitAsync();
+}
--- src/AluraChallengeBackEnd.Api/Program.cs
+++ src/AluraChallengeBackEnd.Api/Program.cs
@@ -8,12 +8,14 @@ builder.Services.AddDbContext<AppDbContext>((provider, options) =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddScoped<IIncomeRepository, IncomeRepository>();
+builder.Services.AddScoped<IExpenditureRepository, ExpenditureRepository>();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
