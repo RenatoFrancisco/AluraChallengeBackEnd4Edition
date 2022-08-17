a3a4b4499c7b1606d06733ca6fa9d86f09f62a7a
RenatoFrancisco <renatofrancisco.s@gmail.com>
2022-08-16 23:18:41 -0300

    Script for populating categories of expenditure

---
+++ sql/INSERT_CategoriesExpenditure.sql
@@ -0,0 +1,8 @@
+INSERT INTO "CategoriesExpenditure" VALUES('73d0f907-6642-42b4-81eb-e5538bd9c584', 'Alimentação');
+INSERT INTO "CategoriesExpenditure" VALUES('c27f8417-1aaf-4be7-91aa-e975e528e85f', 'Saúde');
+INSERT INTO "CategoriesExpenditure" VALUES('1350e4d4-6a1a-483d-881c-4c0a607dfd4b', 'Moradia');
+INSERT INTO "CategoriesExpenditure" VALUES('1ae8d653-944f-4e32-830c-86bd0c230f7e', 'Transporte');
+INSERT INTO "CategoriesExpenditure" VALUES('771006c6-3039-433d-b15f-8bc6cdf8c66d', 'Educação');
+INSERT INTO "CategoriesExpenditure" VALUES('893954ca-5468-4fbd-9ec3-81a2e6aa9afc', 'Lazer');
+INSERT INTO "CategoriesExpenditure" VALUES('35c2b319-1457-4464-ae24-e4fb2dd7338f', 'Imprevistos');
+INSERT INTO "CategoriesExpenditure" VALUES('13cac28d-6f76-428f-8a18-4358037e32ba', 'Outras');
