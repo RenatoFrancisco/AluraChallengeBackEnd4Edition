c56ec64c4d79cb84bdfa34482b38392893ac8a7b
renatofrancisco <renatofrancisco.s@gmail.com>
2022-09-05 23:07:24 -0300

    Implementing the class StartupTest

--- src/AluraChallengeBackEnd.Api/StartupTest.cs
+++ src/AluraChallengeBackEnd.Api/StartupTest.cs
@@ -2,4 +2,26 @@
 
 public class StartupTest
 {
+    public IConfiguration Configuration { get; init; }
+
+    public StartupTest(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+    }
+
+    public void Configure(WebApplication app, IWebHostEnvironment environment)
+    {
+        if (app.Environment.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+    }
 }
