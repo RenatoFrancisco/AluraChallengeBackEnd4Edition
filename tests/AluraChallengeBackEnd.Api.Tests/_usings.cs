a420fb35d1b5e6b29c8a7aae47efe9b576a7cdf7
renatofrancisco <renatofrancisco.s@gmail.com>
2022-09-08 22:45:24 -0300

    Creating first integration test for the Incomecontroller

--- tests/AluraChallengeBackEnd.Api.Tests/_usings.cs
+++ tests/AluraChallengeBackEnd.Api.Tests/_usings.cs
@@ -1,4 +1,9 @@
 global using Xunit;
+global using Xunit.Sdk;
+global using System.Net;
+global using Xunit.Abstractions;
 global using Microsoft.AspNetCore.Hosting;
 global using Microsoft.AspNetCore.Mvc.Testing;
+global using AluraChallengeBackEnd.Domain.Entities;
 global using AluraChallengeBackEnd.Api.Tests.Config;
+global using AluraChallengeBackEnd.Api.Tests.Attributes;
